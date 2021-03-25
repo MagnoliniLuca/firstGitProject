cb560500cb6b0ecd9606822ad709570e2bf01af6
MagnoliniLuca <lmagnolini66@gmail.com>
2021-03-25 09:14:08 +0100

    Added comment (line mail from rep1-mach1

--- letsgo.cpp
+++ letsgo.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main(){
+int main(){ //Another comment from reposiory 1 (machine 1)
 cout << "ciao"<< endl;
 cout <<" Added line"" <<endl;
 cout >>"working on a new feature 1.." <<endl;
